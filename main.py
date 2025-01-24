8bf690ea0dd1b21bb25a2e1cf130428e965f7d82
alex <alex.gusain@gmail.com>
2025-01-23 21:38:21 -0500

    -

--- main.py
+++ main.py
@@ -12,3 +12,13 @@ data = yf.download(
 )
 
 print(data)
+
+# [*********************100%***********************]  1 of 1 completed
+# Price                           Close        High         Low        Open   Volume
+# Ticker                           AAPL        AAPL        AAPL        AAPL     AAPL
+# Datetime                                                                          
+# 2025-01-23 14:30:00+00:00  225.279907  226.259995  224.500000  224.729996  2920044
+# 2025-01-23 14:35:00+00:00  225.539993  225.779999  224.682007  225.270004   856159
+
+for index, row in data.iterrows():
+    print(f"Time: {index}, Close: {row['Close']}, High: {row['High']}, Low: {row['Low']}, Open: {row['Open']}, Volume: {row['Volume']}")
